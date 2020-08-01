bc9655f971b1861f0cc38797f27b0c7d07be0cdd
Md. Rashadul Islam <rashadpust@gmail.com>
2020-08-01 22:12:31 +0600

    added strpie payment backend server and added with client

---
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
+
+
+const StripeCheckoutButton = ({ price }) => {
+        const priceForStripe = price * 100;
+        const publishableKey = "pk_test_bUGSkooKzcy1ct3SR4vccjjD00NYhSHUM0"
+
+        const onToken = token => {
+           axios({
+               method: 'post',
+               url: 'payment',
+               data: {
+                   amount: priceForStripe,
+                   token
+               }
+           }).then(res => {
+               alert("Payment successful!");
+           }).catch(error=> {
+               console.log("payment error: ", JSON.parse(error));
+
+               alert("There is an issue with your payment. Please try later.")
+           })
+        }
+
+        return (
+            <StripeCheckout
+                label= "Pay Now"
+                name='Barishal Enterprize'
+                billingAddress
+                shippingAddress
+                image= "https://svgshare.com/i/CUz.svg"
+                description={`Your total is ${price}`}
+                panelLabel='Pay Now'
+                token= {onToken}
+                stripeKey={publishableKey}
+                />
+        )
+}
+
+export default StripeCheckoutButton;
